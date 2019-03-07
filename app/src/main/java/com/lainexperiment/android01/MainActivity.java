089b4c8f42ad24d2231b33a9909649d09134ff21
Hamid Hashemi <hashemi.h1375@gmail.com>
2019-03-07 23:18:22 +0330

    add MessageController & MainActivity Updated

--- app/src/main/java/com/lainexperiment/android01/MainActivity.java
+++ app/src/main/java/com/lainexperiment/android01/MainActivity.java
@@ -1,20 +1,98 @@
 package com.lainexperiment.android01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements EventObserver {
+    private static final String TAG = "MainActivity";
+    private static final String SAVED_INTEGER_LIST = "savedIntegerList";
+
+    private NotificationCenter notificationCenter;
+    private MessageContrgit oller messageController;
+
+    private Button clearButton;
+    private Button refreshButton;
+    private Button getButton;
+    private LinearLayout dataListLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationCenter = NotificationCenter.getInstance();
+        messageController = MessageController.getInstance(this);
+        notificationCenter.register(EventType.DATA_LOADED, this);
+
+        initUI();
     }
 
-    public void clearLayout(View view) {
-        LinearLayout linearLayout = findViewById(R.id.linear_layout);
-        linearLayout.removeAllViews();
+    private void initUI() {
+        clearButton = findViewById(R.id.clear_btn);
+        refreshButton = findViewById(R.id.refresh_btn);
+        getButton = findViewById(R.id.get_btn);
+        dataListLayout = findViewById(R.id.data_layout);
+        showData();
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataListLayout.removeAllViews();
+                messageController.clearData();
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: called refresh");
+                messageController.fetch(true);
+            }
+        });
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: called get");
+                messageController.fetch(false);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        notificationCenter.unregister(EventType.DATA_LOADED, this);
+    }
+
+    @Override
+    public void update(Event event) {
+        Log.d(TAG, "update: called");
+        showData();
+    }
+
+    private void showData() {
+        dataListLayout.removeAllViews();
+        ArrayList<Integer> data = messageController.getData();
+        Log.d(TAG, "showData: called with " + data.size());
+        for (Integer dataItem: data) {
+            dataListLayout.addView(getItemView(dataItem));
+        }
+    }
+
+    private TextView getItemView(Integer dataItem) {
+        TextView itemView = new TextView(this);
+        itemView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(8,8,8,0);
+        itemView.setLayoutParams(params);
+        itemView.setText(dataItem.toString());
+        itemView.setTextColor(getResources().getColor(R.color.colorAccent));
+        return itemView;
     }
 }
---
+++ app/src/main/java/com/lainexperiment/android01/MessageController.java
@@ -0,0 +1,73 @@
+package com.lainexperiment.android01.;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public class MessageController {
+
+    private ConnectionManager connectionManager;
+    private StorageManager storageManager;
+    private NotificationCenter notificationCenter;
+
+    public ArrayList<Integer> getData() {
+        return data;
+    }
+
+    private ArrayList<Integer> data = new ArrayList<>();
+
+    private static MessageController single_instance = null;
+
+    private MessageController(Context context) {
+        connectionManager = new ConnectionManager();
+        storageManager = new StorageManager(context);
+        notificationCenter = NotificationCenter.getInstance();
+    }
+
+    public static MessageController getInstance(Context context) {
+        if (single_instance == null)
+            single_instance = new MessageController(context);
+
+        return single_instance;
+    }
+
+    public void fetch(Boolean fromCache) {
+        int last = data.isEmpty() ? 0 : data.get(data.size() - 1);
+        if (fromCache) {
+            storageManager.load(last, new DataLoadCallback() {
+                @Override
+                public void onDataLoaded(int[] result) {
+                    if (result != null) {
+                        clearData();
+                        for (int aLoadedData : result) {
+                            data.add(aLoadedData);
+                        }
+                        notificationCenter.dataLoaded();
+                    }
+                }
+            });
+        } else {
+            connectionManager.load(last, new DataLoadCallback() {
+                @Override
+                public void onDataLoaded(int[] result) {
+                    if (result != null) {
+                        clearData();
+                        for (int i = 1; i <= result[9]; i++) {
+                            data.add(i);
+                        }
+                        notificationCenter.dataLoaded();
+                        storageManager.save(result[result.length - 1]);
+                    }
+                }
+            });
+        }
+    }
+
+    private void loadData(int[] loadedData) {
+
+    }
+
+    public void clearData() {
+        data.clear();
+    }
+}
